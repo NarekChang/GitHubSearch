bd8c3a9bf1a5da74339c53000c731ac35b34a0ec
Чанглян Нарек Давидович <NDChanglyan@sberbank.ru>
2022-01-30 18:28:19 +0300

    api

---
+++ src/api/config.js
@@ -0,0 +1,9 @@
+export const baseUrl = "https://api.github.com";
+
+export const usersUrl = "/users";
+export const issuesUrl = "/issues";
+export const repositoriesUrl = "/repositories";
+
+export const searchUrl = `${baseUrl}/search`;
+
+export const sort = "sort=created&order=asc";
